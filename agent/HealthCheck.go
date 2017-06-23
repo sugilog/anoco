b27ae914f1802d5d28927cbd55a2d8dea5cec363
sugilog <sugilog@gmail.com>
2017-06-23 20:59:12 +0900

    use Handler

--- agent/HealthCheck.go
+++ agent/HealthCheck.go
@@ -2,13 +2,8 @@ package agent
 
 import (
 	"net/http"
-
-	"github.com/vmihailenco/msgpack"
 )
 
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	// No case to return error for OK
-	b, _ := msgpack.Marshal(&Result{Status: "OK"})
-	w.WriteHeader(200)
-	w.Write(b)
+	SuccessResult(w, Result{Status: "OK"})
 }
