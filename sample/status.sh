9a750dfa5c4e4425a7014e6b700af7b6fc6e99c9
sugilog <sugilog@gmail.com>
2017-06-24 07:25:39 +0900

    do not use tmpfile, use stdin for msgpack-cli

--- sample/status.sh
+++ sample/status.sh
@@ -1,2 +1,1 @@
-bash -c -v "curl -D /dev/stderr \"localhost:15525/job/status?uuid=$1\"" | tee /tmp/anoco.status.bin
-bash -c -v "msgpack-cli decode /tmp/anoco.status.bin"
+bash -c -v "curl -D /dev/stderr \"localhost:15525/job/status?uuid=$1\" | msgpack-cli decode"
