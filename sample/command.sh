5297870db35e0980dfa44e0ec9f045ff9f5300e8
sugilog <sugilog@gmail.com>
2017-06-24 09:31:33 +0900

    use stdin for curl data

--- sample/command.sh
+++ sample/command.sh
@@ -2,7 +2,7 @@ type=$1
 
 if [ -f sample/command.$type.json ];
 then
-	bash -c -v "msgpack-cli encode sample/command.$type.json | curl -D /dev/stderr localhost:15525/job -XPOST -d@/tmp/anoco.bin | msgpack-cli decode"
+	bash -c -v "msgpack-cli encode sample/command.$type.json | curl -D /dev/stderr localhost:15525/job -XPOST -d @- | msgpack-cli decode"
 else
   echo 'Invalid type given'
   exit 1
